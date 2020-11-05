4e8a96bae67d3c7ed8503e9aa40776255c7993f6
ronald cortez <ronaldcortez@ronalds-MacBook-Air.local>
2020-11-04 22:46:12 -0500

    testing

---
+++ asp_project/Pages/About.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace MyApp.Namespace
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public void Btn_click()
+        {
+            // Console.WriteLine("click");
+            Debug.WriteLine("click");
+
+        }
+    }
+  
+
+
+}
