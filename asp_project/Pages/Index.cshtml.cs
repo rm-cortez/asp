168e66a36c9a1f4c0a0c2ef2d662f510628ede3a
ronald cortez <ronaldcortez@ronalds-MacBook-Air.local>
2020-11-05 23:30:43 -0500

    Added configuration functionality

--- asp_project/Pages/Index.cshtml.cs
+++ asp_project/Pages/Index.cshtml.cs
@@ -2,41 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Data.OleDb;
-using System.Data.Odbc;
 using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace asp_project.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration Configuration;
+        public string name;
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            Configuration = configuration;
         }
 
         public void OnGet()
         {
+            Console.WriteLine(Configuration["ConnectionStrings:myDb1"]);
+
+            name = "ronald";
+
+
+        //public string connString = Configuration["ConnectionStrings:myDb1"];
 
         }
 
-        public void Btn_click()
-        {
 
-        }
        
-        public string name = "ronald";
 
-        public string conn = "Provider=MySqlProvider;Data Source=18.221.52.182;" +
-                    "User id=root;Password=RCSH3llo123;";
-        //OleDbConnection connection = new OleDbConnection(ConnectionString);
-
-        //connection.Open();
 
     }
 }
