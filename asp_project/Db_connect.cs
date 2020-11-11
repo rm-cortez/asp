44b3e19eeb412bf52e6b3b052a6be69f29ffbd1d
ronald cortez <ronaldcortez@ronalds-MacBook-Air.local>
2020-11-10 22:18:42 -0500

    refactored db connection

---
+++ asp_project/Db_connect.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace asp_project
+{
+    public class Db_connect
+    {
+
+        private readonly IConfiguration Configuration;
+        public string connString;
+        public SqlConnection connection;
+
+        public Db_connect(IConfiguration configuration)
+        {
+            Configuration = configuration;
+           
+
+        }
+
+        public string exec_query(string query,string type = "select")
+        {
+            connString = Configuration["ConnectionStrings:myDb1"];
+            connection = new SqlConnection(connString);
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open();
+
+            if(type.Equals("select"))
+            {
+                string retSelect = "";
+
+                SqlDataReader reader = command.ExecuteReader();
+                
+                while (reader.Read())
+                {
+                    retSelect += $" " +
+                        $"{reader.GetName(0)} :{reader.GetValue(0)} " +
+                        $"-- " +
+                        $"{reader.GetName(1)}:{reader.GetValue(1)} \n";
+
+                }
+
+                close_conn();
+                return retSelect;
+
+            }
+            else
+            {
+                //command.CommandType = CommandType.StoredProcedure;
+                command.ExecuteNonQuery();
+                close_conn();
+                return "";
+            }
+
+
+            
+        }
+
+        
+
+        public void close_conn()
+        {
+            connection.Close();
+            connection.Dispose();
+        }
+    }
+}
--- asp_project/Pages/Index.cshtml.cs
+++ asp_project/Pages/Index.cshtml.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
+
 
 namespace asp_project.Pages
 {
@@ -14,53 +14,26 @@ namespace asp_project.Pages
     {
 
         private readonly ILogger<IndexModel> _logger;
-        private readonly IConfiguration Configuration;
+        
         public string name;
-        public string connString;
+        private readonly IConfiguration Configuration;
+
 
         public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
             Configuration = configuration;
+            
         }
 
         public void OnGet()
         {
-            //Console.WriteLine(Configuration["ConnectionStrings:myDb1"]);
+          
 
-            name = "ronald";
+            Db_connect dbconnect = new Db_connect(Configuration);
+            name = dbconnect.exec_query("select * from technologies");
 
-            connString = Configuration["ConnectionStrings:myDb1"];
-
-            Console.WriteLine(connString);
-
-
-
-
-            SqlConnection connection = new SqlConnection(connString);
-
-            SqlCommand command = new SqlCommand("select * from table_1", connection);
-
-            connection.Open();
-
-            SqlDataReader reader = command.ExecuteReader();
-
-            while (reader.Read())
-            {
-                Console.WriteLine($" " +
-                    $"{reader.GetName(0)} :{reader.GetValue(0)} " +
-                    $"-- " +
-                    $"{reader.GetName(1)}:{reader.GetValue(1)} ");
-
-            }
-            connection.Close();
-            connection.Dispose();
-            //    Console.WriteLine("sql: ", connection);
-
-
-
-
-
+           
 
 
         }
