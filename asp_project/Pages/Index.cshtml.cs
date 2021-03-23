a7b860964c5071f10da2761df202626a1ca52ee1
ronald cortez <ronaldcortez@ronalds-MacBook-Air.local>
2021-03-23 08:41:53 -0400

    updated layout

--- asp_project/Pages/Index.cshtml.cs
+++ asp_project/Pages/Index.cshtml.cs
@@ -26,6 +26,7 @@ namespace asp_project.Pages
         public string name;
         public List<Dictionary<string, string>> values;
         public string output;
+        public string content;
 
 
         public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
@@ -37,24 +38,36 @@ namespace asp_project.Pages
 
         public void OnGet()
         {
-          
-
-            Db_connect dbconnect = new Db_connect(Configuration);
-            values = new List<Dictionary<string, string>>();
-            values = dbconnect.exec_query("select * from technologies order by name");
 
 
-            output = System.Net.WebUtility.HtmlDecode(JsonConvert.SerializeObject( values) );
+            //Db_connect dbconnect = new Db_connect(Configuration);
+            //values = new List<Dictionary<string, string>>();
+            //values = dbconnect.exec_query("select * from technologies order by name");
+
+
+            //output = System.Net.WebUtility.HtmlDecode(JsonConvert.SerializeObject( values) );
 
 
             //http get request
-            ////var ts =  getJson();
-            //Task<string> ts =  getJson();
+            //var ts =  getJson();
+            Task<string> ts = getJson();
 
-            //// wait for the task to complete to continue
-            //output = ts.Result;
+            // wait for the task to complete to continue
+            output = ts.Result;
+
+            dynamic obj = JsonConvert.DeserializeObject(output);
 
 
+
+            content = obj.First.content;
+
+            //foreach (dynamic item in obj)
+            //{
+            //    foreach (dynamic it in item)
+            //    {
+            //        Console.WriteLine($"Value: {it.content}");
+            //    }
+            //}
 
         }
 
@@ -63,7 +76,7 @@ namespace asp_project.Pages
             //get request
             HttpClient hp = new HttpClient();
             hp.BaseAddress = new Uri("http://rcsproductions.us/");
-            var response = (await hp.GetAsync("json-builder.php") ).Content.ReadAsStringAsync().Result;
+            var response = (await hp.GetAsync("content-builder.php?type=asp") ).Content.ReadAsStringAsync().Result;
 
             
             return response;
